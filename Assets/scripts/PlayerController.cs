907de49ba47945679c25f8c25b0ba31ddb252eb1
Anurag Dadheech <anuragdadheech1990@gmail.com>
2015-09-26 13:58:45 +0530

    Added gitignore and Remove ignored files

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody rbBall;
+	// Use this for initialization
+	void Start () {
+		rbBall = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		Vector3 ballMovement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rbBall.AddForce (ballMovement * speed);
+	}
+}
